c253d6410cf83585f6052f38b184e40ca5e96e07
TDumler74 <tatjana.dumler@gmail.com>
2023-04-07 19:19:00 +0200

    homeworkText

---
+++ src/Aufgabe_1.java
@@ -0,0 +1,24 @@
+public class Aufgabe_1 {
+    public static void main(String[] args) {
+       /* Напишите программу, которая выводит на экран информацию о вас (или выдуманном персонаже)
+        в три строчки:
+
+        "Меня зовут ..."
+        "Мне ... лет"
+        "Я из ..."
+        На месте многоточий должны использоваться переменные!
+
+                */
+        String name="Tatjana";
+        int age=48;
+        String city= "Bielefeld";
+
+        System.out.println("Меня зовут "+name+".");
+        System.out.println("Мне "+age+" лет.");
+        System.out.println("Я из "+city+".");
+        //Меня зовут Tatjana.
+        //Мне48 лет.
+        //Я из Bielefeld.
+
+    }
+}
